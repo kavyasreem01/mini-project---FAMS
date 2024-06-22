149ca925a1d46b22178187043817793a79958a26
kavyasree01 <kavyasreemantipally@gmail.com>
2024-06-22 23:35:11 +0530

    updated-2 project

--- app/src/main/java/com/example/faams/RegisterActivity.kt
+++ app/src/main/java/com/example/faams/RegisterActivity.kt
@@ -1,67 +1,9 @@
-/*package com.example.faams
 
-
-import android.os.Bundle
-import android.widget.Toast
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import com.example.faams.databinding.ActivityRegisterBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
-
-class RegisterActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityRegisterBinding
-    private lateinit var firebaseAuth: FirebaseAuth
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityRegisterBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        firebaseAuth = FirebaseAuth.getInstance()
-
-        binding.textView.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val pass = binding.passET.text.toString()
-            val confirmPass = binding.confirmPassEt.text.toString()
-
-            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
-                if (pass == confirmPass) {
-                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val intent = Intent(this, LoginActivity::class.java)
-                            startActivity(intent)
-                        } else {
-                            try{
-                                throw it.exception!!
-                            }catch (e: FirebaseAuthUserCollisionException) {
-                                Toast.makeText(this, "This email address is already in use. Please use another email or login.", Toast.LENGTH_LONG).show()
-                            } catch (e: Exception) {
-                                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-
-                        }}
-                    }
-                } else {
-                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
-            }
-        }
-    }
-}*/
 package com.example.faams
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.example.faams.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -84,18 +26,21 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
+            val phoneNumber = binding.phoneEt.text.toString()
 
-            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty() && phoneNumber.isNotEmpty()) {
                 if (pass == confirmPass) {
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
                         if (task.isSuccessful) {
                             val user = firebaseAuth.currentUser
                             user?.sendEmailVerification()?.addOnCompleteListener { verifyTask ->
                                 if (verifyTask.isSuccessful) {
+
                                     Toast.makeText(this, "Registration successful. Please check your email for verification.", Toast.LENGTH_LONG).show()
                                     val intent = Intent(this, LoginActivity::class.java)
                                     startActivity(intent)
@@ -122,3 +67,4 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 }
+
